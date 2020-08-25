03d70e29a3e6dff3d2f345cf3a2ef19bac0b1d3e
jmins33 <jmins310@gail.com>
2020-08-25 11:33:52 +0900

    'mypractice'

---
+++ arithmetic/plus.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+
+def plus(a, b):
+    c = a + b
+    return c
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 # main.py dir에서 calculation.py 정의한 함수 불러오기
+# -*- coding:utf-8 -*-
 
 import calculation as cal
+from arithmetic import plus
+from arithmetic import subtract
+from dataPreprocessing import processing
+from dataPreprocessing import importData
 
-a = 3
-b = 4
+a = 30
+b = 40
 
 def main():
-    print('안녕하세요, main입니다')
-    print('a+b=', cal.plus(a,b))
-    print('a-b=', cal.subtract(a,b))
-    print('a*b=', cal.multiple(a,b))
+    print("~~ 사칙 연산을 시작합니다 ~~ ")
+    print('a+b = ', plus.plus(a,b))
+    print('a+b = ', subtract.subtract(a,b))
+    print("~~ 사칙 연산을 종료합니다 ~~ ")
+
+## 데이터 전처리 시작
+data = importData.readData()
+processing.process_data(data)
+
 if __name__ == '__main__':
     main()
